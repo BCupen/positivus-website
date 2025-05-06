8499a87b32f9944ed017c73ca67a7c023858743a
Bry Cupen <bacupen@gmail.com>
2025-05-06 14:56:48 -0400

    feat: complete service cards

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -25,35 +25,33 @@ export const ServiceCard = ({
 }: ServiceCardProps) => {
     return (
         <div
-            className={`border-dark rounded-card shadow-bold h-[300px] border p-12 ${cardStyles} flex justify-between gap-4`}
+            className={`border-dark rounded-card shadow-bold h-[300px] border p-10 ${cardStyles} grid grid-cols-2 gap-4`}
         >
-            <div className="flex w-full flex-col justify-between">
-                <Heading
-                    as="h3"
-                    styles="leading-tight w-full text-wrap"
-                >
-                    <mark className={`rounded px-2 ${titleStyles}`}>
-                        {title}
-                    </mark>
-                </Heading>
+            <Heading
+                as="h3"
+                styles="leading-tight w-full text-wrap col-span-2 lg:col-span-1 order-1"
+            >
+                <mark className={`rounded px-2 ${titleStyles}`}>
+                    {title}
+                </mark>
+            </Heading>
 
-                <button className="flex cursor-pointer items-center gap-4">
-                    <LinkCircle
-                        bgColor={linkStyle.bgColor}
-                        fill={linkStyle.fill}
-                    />
-                    <p
-                        className={`${linkStyle.textColor} hidden lg:block`}
-                    >
-                        Learn More
-                    </p>
-                </button>
-            </div>
+            <button className="order-2 col-span-1 flex cursor-pointer items-center gap-4 max-lg:self-end lg:order-3">
+                <LinkCircle
+                    bgColor={linkStyle.bgColor}
+                    fill={linkStyle.fill}
+                />
+                <p
+                    className={`${linkStyle.textColor} hidden lg:block`}
+                >
+                    Learn More
+                </p>
+            </button>
 
             <img
                 src={imgSrc}
                 alt="service-image"
-                className="h-[100px] w-auto object-cover lg:h-[150px]"
+                className="order-3 row-span-1 h-[100px] w-auto object-cover lg:order-2 lg:row-span-2 lg:h-[150px] lg:self-center"
             />
         </div>
     );
