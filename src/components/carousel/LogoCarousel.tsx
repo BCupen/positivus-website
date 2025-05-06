44d7699a8963b8daa945bacc50f786e8967c1bf6
Bry Cupen <bacupen@gmail.com>
2025-05-05 20:39:08 -0400

    feat: carousel mobile and desktop

--- src/components/carousel/LogoCarousel.tsx
+++ src/components/carousel/LogoCarousel.tsx
@@ -11,13 +11,36 @@ const images = [Amazon, Hubspot, Dribble, Notion, Netflix, Zoom];
 export const LogoCarousel = () => {
     return (
         <section>
-            <div className="fles relative mb-10 h-[70px] w-[100%] overflow-hidden border md:mb-20">
+            <div
+                id="carousel"
+                className="relative mb-10 flex h-[50px] w-[100%] min-w-[600px] overflow-hidden md:mb-20 md:h-[70px] md:min-w-[900px]"
+            >
                 {images.map((image, index) => (
                     <img
                         key={index}
                         src={image}
                         alt={`Logo ${index + 1}`}
-                        className="slide-left absolute top-1/2 left-[100%] h-auto w-[150px] -translate-y-1/2 bg-red-500 grayscale"
+                        style={{
+                            animationDelay: `calc(4s * ${6 - index} * -1)`, // 30s/6 * index
+                        }}
+                        className="slide-left absolute top-1/2 left-[100%] h-auto w-[100px] -translate-y-1/2 grayscale md:w-[150px]"
+                    />
+                ))}
+            </div>
+
+            <div
+                id="carousel"
+                className="relative mb-10 flex h-[50px] w-[100%] min-w-[600px] overflow-hidden md:mb-20 md:hidden md:h-[70px] md:min-w-[900px]"
+            >
+                {images.map((image, index) => (
+                    <img
+                        key={index}
+                        src={image}
+                        alt={`Logo ${index + 1}`}
+                        style={{
+                            animationDelay: `calc((4s * ${6 - index} * -1) + 3s)`, // 30s/6 * index
+                        }}
+                        className="slide-right absolute top-1/2 left-[-100%] h-auto w-[100px] -translate-y-1/2 grayscale md:w-[150px]"
                     />
                 ))}
             </div>
