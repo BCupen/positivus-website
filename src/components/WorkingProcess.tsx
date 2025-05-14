e034a298bf7dd5ba55adef042492cc6c081be073
BCupen <bacupen@gmail.com>
2025-05-14 19:38:42 -0400

    feat: collapsible

--- src/components/WorkingProcess.tsx
+++ src/components/WorkingProcess.tsx
@@ -2,17 +2,17 @@ import {Heading} from "./Heading.tsx";
 
 export const WorkingProcess = () => {
     return <section className="mb-10 flex flex-col gap-20 md:mb-20">
-        <div className="flex items-center justify-center gap-10 max-md:flex-col">
+        <div className="flex items-center gap-10 max-md:flex-col">
             <Heading
                 as="h2"
                 styles="text-dark bg-primary px-2 rounded"
             >
-                Services
+                Our Working Process
             </Heading>
             <p>
-                At our digital marketing agency, we offer a range
-                of services to help businesses grow and succeed
-                online. These services include:
+                Step-by-Step Guide to Achieving
+                <br />
+                Your Business Goals
             </p>
         </div>
     </section>;
