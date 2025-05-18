693ca94507d2d6d6b435e6659b58583fbe253d8b
BCupen <bacupen@gmail.com>
2025-05-18 16:59:44 -0400

    feat: working process section complete

--- src/components/WorkingProcess.tsx
+++ src/components/WorkingProcess.tsx
@@ -1,7 +1,56 @@
 import { Heading } from "./Heading.tsx";
 import { Collapsible } from "./collapsible";
+import { useState } from "react";
+
+const steps = [
+     {
+          stepNumber: "01",
+          title: "Consultation",
+          description:
+               "During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.",
+     },
+     {
+          stepNumber: "02",
+          title: "Research and Strategy Development",
+          description:
+               "We will then develop a strategy to help you achieve your goals. This will include creating a marketing plan, developing a marketing campaign, and implementing a marketing strategy.",
+     },
+     {
+          stepNumber: "03",
+          title: "Implementation",
+          description:
+               "We will then implement your marketing strategy. We will work with you to ensure that your marketing campaign is successful.",
+     },
+     {
+          stepNumber: "04",
+          title: "Monitoring and Optimization",
+          description:
+               "We will monitor your marketing campaign and optimize it to ensure that it is successful.",
+     },
+     {
+          stepNumber: "05",
+          title: "Reporting and Communication",
+          description:
+               "We will report on your marketing campaign and communicate the results to you. We will also provide you with a detailed report on your marketing campaign.",
+     },
+     {
+          stepNumber: "06",
+          title: "Continual Improvement",
+          description:
+               "We will continually improve our services to ensure that you receive the best results.",
+     },
+];
 
 export const WorkingProcess = () => {
+     const [activeStep, setActiveStep] = useState("01");
+
+     const handleActiveStep = (stepNumber: string) => {
+          if (stepNumber === activeStep) {
+               setActiveStep("");
+               return;
+          }
+          setActiveStep(stepNumber);
+     };
      return (
           <section className="mb-10 flex flex-col gap-20 md:mb-20">
                <div className="flex items-center gap-10 max-md:flex-col">
@@ -15,8 +64,15 @@ export const WorkingProcess = () => {
                     </p>
                </div>
 
-               <div>
-                    <Collapsible />
+               <div className="flex flex-col gap-6">
+                    {steps.map((step, index) => (
+                         <Collapsible
+                              key={index}
+                              {...step}
+                              activeStep={activeStep}
+                              setActiveStep={handleActiveStep}
+                         />
+                    ))}
                </div>
           </section>
      );
--- src/components/collapsible/Collapsible.tsx
+++ src/components/collapsible/Collapsible.tsx
@@ -1,33 +1,43 @@
-import { useState } from "react";
 import { MinusIcon, PlusIcon } from "../Icons.tsx";
 
 export interface CollapsibleProps {
+     activeStep: string;
+     setActiveStep: (step: string) => void;
      stepNumber?: string;
      title?: string;
      description?: string;
 }
 
 export const Collapsible = ({
+     activeStep,
+     setActiveStep,
      stepNumber = "01",
      title = "Title",
      description = "Description",
 }: CollapsibleProps) => {
-     const [isActive, setIsActive] = useState(true);
+     const isActive = stepNumber === activeStep;
      return (
           <div
                className={`${isActive ? "bg-primary" : "bg-light"} shadow-bold border-dark lg:rounded-card text-dark rounded-full border p-12`}
           >
-               <div className="flex w-full justify-between">
+               <div className="flex w-full items-center justify-between">
                     <span className="flex items-center gap-5">
                          <h3 className="text-5xl font-medium">{stepNumber}</h3>
                          <h4 className="text-xl font-medium">{title}</h4>
                     </span>
 
-                    <button onClick={() => setIsActive(!isActive)}>
+                    <button
+                         onClick={() => setActiveStep(stepNumber)}
+                         className="cursor-pointer"
+                    >
                          {isActive ? <MinusIcon /> : <PlusIcon />}
                     </button>
                </div>
-               <div>{description}</div>
+               <div
+                    className={`${isActive ? "h-20 border-t border-gray-700" : "h-0"} mt-6 overflow-hidden transition-all duration-200`}
+               >
+                    <p className="py-6">{description}</p>
+               </div>
           </div>
      );
 };
